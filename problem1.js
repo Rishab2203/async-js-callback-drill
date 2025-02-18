fe35fa06b154dec320896a03d4d5bf38e210b7f0
Rishab <rishabbhardwajdev@gmail.com>
2025-02-18 18:22:53 +0530

    createAndDeleteFiles simultaneously

---
+++ problem1.js
@@ -0,0 +1,38 @@
+// Using callbacks and the fs module's asynchronous functions, do the following:
+// 1. Create a directory of random JSON files
+// 2. Delete those files simultaneously
+// */
+const fs = require("fs");
+
+function createAndDeleteFiles(num) {
+  fs.mkdir("./Json", () => {
+    let count = 0;
+    for (let index = 1; index <= num; index++) {
+      fs.writeFile(
+        `./Json/file${index}.json`,
+        JSON.stringify(`i m File${1}`),
+        (err) => {
+          if (err) {
+            console.error(err.message);
+          }
+          console.log(`file ${index} created`);
+        }
+      );
+      count++;
+    }
+    if (count === num) {
+      for (let index = 1; index <= num; index++) {
+        fs.rm(`./Json/file${index}.json`, (err) => {
+          if (err) {
+            console.error(err.message);
+          }
+          console.log(`file ${index}removed`);
+        });
+      }
+    }
+  });
+}
+
+createAndDeleteFiles(50);
+
+module.exports = createAndDeleteFiles;
